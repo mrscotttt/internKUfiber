82300169992fbbf7eca9aaebd599fdb9843ffa87
mrscotttt <52620083+mrscotttt@users.noreply.github.com>
2021-07-07 03:04:18 +0700

    Update database.js

--- server/server/database/database.js
+++ server/server/database/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Connect = async () => {
     //mongodb://mongo:27017/expressmongo
     //mongoose.connect('mongodb://localhost:27017/data', {
-    mongoose.connect('mongodb://mongo:27017/expressmongo', {
+    mongoose.connect("mongodb+srv://admin_ocs:kasetocs@cluster0.kfw9q.mongodb.net/data?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
